951bf446704f0dc71fa16aad0d35370c20206de5
Andrew N <aninneman@gmail.com>
2018-02-09 16:57:10 -0600

    Add import from actions

--- src/views/ProductEdit.js
+++ src/views/ProductEdit.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import {connect} from 'react-redux'
+import {editProduct, deleteProduct} from '../store/actions';
 class ProductEdit extends Component {
     constructor (props){
         super(props)
